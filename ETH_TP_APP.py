8b12275dcf76ed6b945a9eb020056244a7b2c5ff
1waytwitch-ui <1waytwitch@gmail.com>
2025-08-12 14:14:32 +0200

    Ajout de l'application Streamlit ETH TP APP

---
+++ ETH_TP_APP.py
@@ -0,0 +1,9 @@
+# Contenu du fichier ETH_TP_APP.py
+
+import streamlit as st
+
+# Votre logique d'application Streamlit ici
+
+st.title('ETH TP APP')
+
+# Ajoutez d'autres éléments de l'application ici
